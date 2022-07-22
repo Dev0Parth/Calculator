4da9e982880007eed6fecbfc9e972d93a62477df
Dev0Parth <parth.patel00@outlook.com>
2022-07-22 14:02:29 +0530

    v2 update 	modified:   build/asset-manifest.json 	modified:   build/index.html 	renamed:    build/static/js/main.a9041cf6.js -> build/static/js/main.c3f20f3f.js 	renamed:    build/static/js/main.a9041cf6.js.LICENSE.txt -> build/static/js/main.c3f20f3f.js.LICENSE.txt 	renamed:    build/static/js/main.a9041cf6.js.map -> build/static/js/main.c3f20f3f.js.map 	modified:   build/styles.css 	modified:   public/styles.css 	modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p>&#169; {new Date().getFullYear()}</p>
+            <p>&#169; {new Date().getFullYear()} | made with <span>&#10084;</span> by Parth Patel</p>
         </footer>
     );
 }
