122b85102616bd036d7d6d76a964cd4ac299d444
Dev0Parth <parth.patel00@outlook.com>
2022-07-22 13:57:23 +0530

    bug fixed 	modified:   build/asset-manifest.json 	modified:   build/index.html 	deleted:    build/static/js/main.20400958.js 	deleted:    build/static/js/main.20400958.js.map 	new file:   build/static/js/main.a9041cf6.js 	renamed:    build/static/js/main.20400958.js.LICENSE.txt -> build/static/js/main.a9041cf6.js.LICENSE.txt 	new file:   build/static/js/main.a9041cf6.js.map 	modified:   src/components/App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./Header";
 import Calculator from "./Calculator";
 import Footer from "./Footer";
 
 function App() {
   return (
     <>
-      <Header />
       <Calculator />
       <Footer />
     </>
